1c37939c24000cc829e43a6d1bcb8c1ac8b439d1
AIPC <cy950812@gmail.com>
2025-08-18 21:53:11 +0800

    add user button

--- random/src/main.c
+++ random/src/main.c
@@ -4,7 +4,7 @@
  * @Author: AIPC cy950812@gmail.com
  * @Date: 2025-08-16 22:25:06
  * @LastEditors: AIPC cy950812@gmail.com
- * @LastEditTime: 2025-08-17 15:08:44
+ * @LastEditTime: 2025-08-18 21:05:47
  * @FilePath: _zephyr_stm32f469i_zephyr_samples_myapp_random_src_main.c
  * @Description:
  *
@@ -26,47 +26,76 @@
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
 
-/* The devicetree node identifier for the "led0" alias. */
+/* The devicetree node identifier for the "led" alias. */
 #define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+
+#define USER_BUTTON_NODE DT_ALIAS(sw0)
 
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec user_button = GPIO_DT_SPEC_GET(USER_BUTTON_NODE, gpios);
 
 int main(void)
 {
 	int ret;
-	bool led_state = true;
+	bool led0_state = true;
+	bool user_button_state = false;
 
-	if (!gpio_is_ready_dt(&led)) {
+	if (!gpio_is_ready_dt(&led0)) {
 		return 0;
 	}
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
 		return 0;
 	}
 
+	if (!gpio_is_ready_dt(&led1)) {
+		return 0;
+	}
+
+	ret = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return 0;
+	}
+
+	int delay = 0;
 	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return 0;
+
+		if (delay%10 == 0) {
+			bool button_state = gpio_pin_get_dt(&user_button);
+			if (user_button_state != button_state) {
+				user_button_state = button_state;
+				gpio_pin_toggle_dt(&led1);
+			}
 		}
 
-		led_state = !led_state;
-		printf("LED state: %s\n", led_state ? "ON" : "OFF");
+		if (delay > SLEEP_TIME_MS) {
+			delay = 0;
+			ret = gpio_pin_toggle_dt(&led0);
+			if (ret < 0) {
+				return 0;
+			}
 
-		uint32_t random_value = sys_rand32_get();
-		printf("Random value: %u\n", random_value);
+			led0_state = !led0_state;
+			printf("LED state: %s\n", led0_state ? "ON" : "OFF");
 
-		double random_double = ((double)random_value / UINT32_MAX + 1.0);
-		printf("Random double: %f\n", random_double);
+			uint32_t random_value = sys_rand32_get();
+			printf("Random value: %u\n", random_value);
 
-		say_hello();
+			double random_double = ((double)random_value / UINT32_MAX + 1.0);
+			printf("Random double: %f\n", random_double);
 
-		k_msleep(SLEEP_TIME_MS);
+			say_hello();
+		}
+
+		k_msleep(1);
+		delay++;
 	}
 	return 0;
 }
